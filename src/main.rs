580a26d83b0294fe8cdb39b3336777d0377f78c8
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-12-22 17:27:04 -0500

    Added error handling for extracting command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     //read command line arguments and collect the values into a vector
     let args: Vec<String> = env::args().collect();
 
     //passing a reference to args
-    let config = Config::new(&args);
+    //handling an error if our Config build function fails and display readable error message to end user
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -22,12 +27,14 @@ struct Config {
 }
 //By cloning, you create a new String that is a copy of the string data. This means Config now owns its data independently of the args vector
 impl Config {
-    fn build(args: &Vec<String>) -> Config {
+    //The error is a string slice, not an owned String. This is often used for error messages to avoid the overhead of heap allocation associated with String.
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
