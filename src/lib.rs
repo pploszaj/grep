5a955c6a202404c55bed16435e07e56a76b2146c
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-12-27 16:41:32 -0500

    Add a case insensitive search function and read environment variables to decide which search func to run

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 pub struct Config {
     pub query: String,
-    pub file_path: String
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 //By cloning, you create a new String that is a copy of the string data. This means Config now owns its data independently of the args vector
 impl Config {
@@ -14,19 +16,37 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config { query, file_path, ignore_case})
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let content = fs::read_to_string(config.file_path)?;
-    for line in search(&config.query, &content) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search(&config.query, &content)
+    };
+
+    for line in results{
         println!("{line}");
     }
     Ok(())
 }
 
 //Rust has a helpful method to handle line-by-line iteration of strings, conveniently named lines
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
